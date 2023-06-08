8ea70a2327535b49d26b8bfc4b5693c82249ef65
jeandmalves <jeandmalves@gmail.com>
2023-06-08 15:53:23 -0300

    Servidor e player funfando

---
+++ game_server.py
@@ -0,0 +1,75 @@
+import socket
+import threading
+import random
+from player import Player
+
+class GameServer(threading.Thread):
+    """
+        Classe responsável por iniciar o servidor de 1 jogo, 
+        esperar por conexões de "jogadores" e 
+        criar 1 thread por jogador através da classe 'PlayerThread', 
+        salvando os jogadores presentes nessa instância de servidor no atributo players 
+    """
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.HOST = 'localhost'
+        self.PORT = random.randint(9000, 10000)
+        # self.players = []
+        self.players = {}
+        self.lock = threading.Lock() # Objeto de "tranca" -> Concorrência
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Usando TCP  
+        print(f'Jogo iniciado em {self.HOST}:{self.PORT}')
+    
+    def run(self):
+        self.server.bind((self.HOST, self.PORT))
+        self.server.listen()
+        while True:
+            conn, addr = self.server.accept()
+            thread = PlayerThread(conn, addr, self)
+            thread.start()
+
+
+class PlayerThread(threading.Thread):
+    """
+        Classe responsável por lidar com as threads de cada player
+        Cada player estará associado a um jogo, podem haver diversos players em um único jogo 
+    """
+    def __init__(self, conn, addr, game):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+        self.game = game
+
+    def run(self):
+        with self.game.lock:
+            self.game.players[self.conn] = Player(self.conn)
+        
+        print('Connected:', self.addr)
+
+        while True:
+            data = self.conn.recv(1024).decode('utf-8')
+            if not data: break
+            elif data[0:3] == "-n ": # Prefixo que indica que a mensagem é para colocar um novo nome
+                name = data[3:]
+                player = self.game.players[self.conn]
+                player.setName(name)
+            else:
+                player = self.game.players[self.conn]       
+                
+                print(f'<{player.getName()}>: {data}')
+                
+                with self.game.lock:
+                    for player in self.game.players:
+                        if not (player == self.conn): # Mando para todos os usuários, exceto o próprio usuário que enviou a mensagem                    
+                            player.sendall(data.encode('utf-8'))
+
+        with self.game.lock:
+            self.game.players.remove(self.conn)
+            
+        self.conn.close()
+        print('Disconnected:', self.addr)
+                
+
+if __name__ == "__main__":
+    game_thread = GameServer()
+    game_thread.start()
---
+++ player.py
@@ -0,0 +1,65 @@
+import socket
+import threading 
+import sys
+
+class ReceiveThread(threading.Thread):
+    """
+        Classe (thread) usada para receber mensagens vindas do servidor
+    """
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        
+    def run(self):
+        while True:
+            data = self.conn.recv(1024).decode('utf-8')
+            if not data:
+                break
+            print("")
+            print('Received:', data)
+            
+class PlayerClient(threading.Thread):
+    """
+        Classe responsável mandar mensagens para o servidor,
+        também inicia a classe ReceiveThread (receber mensagens do servidor)
+    """
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.HOST = 'localhost'
+        self.PORT = int(sys.argv[1])
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
+        self.client.connect((self.HOST, self.PORT))
+        print(f'Conectado ao servidor {self.HOST}:{self.PORT}')
+    
+    def run(self):
+        message_thread = ReceiveThread(self.client)
+        message_thread.start()
+        self.setName()
+        while True:
+            message = input('Enter a message (or "quit" to exit): ')
+            if message == 'quit':
+                break
+            self.client.sendall(message.encode('utf-8'))
+        self.client.close()
+    
+    def setName(self):
+        prefix = "-n "
+        message = input("Digite seu nome/apelido: ")
+        send = prefix + message
+        self.client.sendall(send.encode('utf-8'))
+      
+class Player:
+    def __init__(self, conn):
+        self.conn = conn
+        self.name = None
+        self.points = None
+        
+    def setName(self, name):
+        self.name = name
+    
+    def getName(self):
+        return self.name
+      
+if __name__ == "__main__":
+    player_thread = PlayerClient()
+    player_thread.start()
